82069c9795156fa51b6fdb4494e07f8ec6b1a51a
Ryan <vellabonelloryan@gmail.com>
2024-04-08 16:02:56 +0200

    implements FilamentUser contract

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 use App\Enums\AccountType;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasName
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -30,4 +33,16 @@ class User extends Authenticatable
         'password' => 'hashed',
         'account_type' => AccountType::class,
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+
+        // return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+    }
+
+    public function getFilamentName(): string
+    {
+        return $this->username;
+    }
 }
