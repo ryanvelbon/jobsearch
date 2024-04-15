549e66d3e481186c1b1e8158725256d5a488abe4
Ryan <vellabonelloryan@gmail.com>
2024-04-15 13:42:34 +0200

    feat: header with mobile menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,77 @@
-<nav class="bg-gray-800 text-white p-4">
-    <div class="flex justify-between">
-        <div>
-            <a href="{{ route('home') }}">Home</a>
+<header x-data="{ open: false}" class="bg-white">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+        <div class="flex gap-x-12">
+            <a wire:navigate href="{{ route('home') }}">
+                <x-logo class="h-8 text-primary-600" />
+            </a>
+            <a href="#" class="hidden md:block text-sm font-semibold leading-6 text-gray-900">Link</a>
+            <a href="#" class="hidden md:block text-sm font-semibold leading-6 text-gray-900">Link</a>
         </div>
-        <div>
-            @if (Route::has('login'))
-                @auth
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit">
-                            <span>Sign out</span>
-                        </button>
-                    </form>
-                @else
-                    <a href="{{ route('login') }}" class="text-white hover:text-primary-400">Log in</a>
+        <div class="flex flex-1 items-center justify-end gap-x-6">
+            @auth
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="text-sm font-semibold leading-6 text-gray-900">
+                        <span>Sign out</span>
+                    </button>
+                </form>
+            @else
+                <a href="{{ route('login') }}" class="hidden lg:block text-sm font-semibold leading-6 text-gray-900">Sign in</a>
+                <a href="{{ route('register') }}" class="hidden lg:block text-sm font-semibold leading-6 text-gray-900">Register</a>
+            @endauth
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 btn btn-primary">Register</a>
-                    @endif
-                @endauth
-            @endif
+            <a href="#" class="btn btn-primary">Post a job</a>
+        </div>
+        <div class="flex lg:hidden">
+            <button @click="open = true" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                <span class="sr-only">Open main menu</span>
+                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
+        </div>
+    </nav>
+    <!-- Mobile menu, show/hide based on menu open state. -->
+    <div x-cloak x-show="open" class="lg:hidden" role="dialog" aria-modal="true">
+        <!-- Background backdrop, show/hide based on slide-over state. -->
+        <div class="fixed inset-0 z-10"></div>
+        <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div class="flex items-center gap-x-6">
+                <a wire:navigate href="{{ route('home') }}" class="-m-1.5 p-1.5">
+                    <span class="sr-only">Your Company</span>
+                    <x-logo class="h-8 text-primary-600" />
+                </a>
+                <a href="#" class="ml-auto btn btn-primary">Post a job</a>
+                <button @click="open = false" type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                    <span class="sr-only">Close menu</span>
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+            <div class="mt-6 flow-root">
+                <div class="-my-6 divide-y divide-gray-500/10">
+                    <div class="space-y-2 py-6">
+                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Link</a>
+                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Link</a>
+                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Link</a>
+                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Link</a>
+                    </div>
+                    <div class="py-6">
+                        @auth
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                    <span>Sign out</span>
+                                </button>
+                            </form>
+                        @else
+                            <a href="{{ route('login') }}" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Sign in</a>
+                            <a href="{{ route('register') }}" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Register</a>
+                        @endauth
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</nav>
+</header>
