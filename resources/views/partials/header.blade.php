b38784ee33933190773df541fcf0c4ba15569002
Ryan <vellabonelloryan@gmail.com>
2024-04-16 22:14:46 +0200

    style: fixes profile dropdown flicker using x-cloak

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -19,7 +19,7 @@
                         </button>
                     </div>
 
-                    <div x-show="open" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1"
+                    <div x-cloak x-show="open" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1"
                         x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95"
                     >
                         <!-- Active: "bg-gray-100", Not Active: "" -->
