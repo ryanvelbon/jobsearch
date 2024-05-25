bd93fa900ca5bbd88f99c6f69707959ee8e2400f
Ryan <vellabonelloryan@gmail.com>
2024-05-25 19:37:43 +0200

    refactor: new anonymous Blade component for filter dropdowns

--- resources/views/livewire/job-listing-index.blade.php
+++ resources/views/livewire/job-listing-index.blade.php
@@ -20,32 +20,17 @@
                     </div>
                 </div>
                 <div class="mt-3">
-                    <div x-data="{ open: false }" class="relative inline-block text-left">
-                        <div>
-                            <button @click="open = !open" type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-full px-4 py-2 text-sm border-primary-400 hover:border-white border-2 shadow-sm" :class="open ? 'bg-white text-gray-700' : 'text-primary-400 hover:text-white'">
-                                All work types
-                                <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                                </svg>
-                            </button>
-                        </div>
-                        <div
-                            x-show="open"
-                            x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95"
-                            @click.outside="open = false"
-                            class="absolute left-0 z-10 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" tabindex="-1"
-                        >
-                            <div class="pl-8 py-6 space-y-2" role="none">
-                                @foreach($options['workTypes'] as $option)
-                                    <div class="flex items-center group">
-                                        <input wire:model.live="selectedWorkTypes" id="wt-{{ $option->name }}" type="checkbox" value="{{ $option->value }}" class="h-5 w-5 rounded border-gray-500 group-hover:border-gray-700 text-primary-600 focus:ring-primary-600 cursor-pointer">
-                                        <label for="wt-{{ $option->name }}" class="pl-4 py-2 select-none w-full text-gray-600 group-hover:text-gray-800 text-lg cursor-pointer">{{ $option->value }}</label>
-                                    </div>
-                                @endforeach
+                    <x-dropdown label="All work types">
+                        @foreach($options['workTypes'] as $option)
+                            <div class="flex items-center group">
+                                <input wire:model.live="selectedWorkTypes" id="wt-{{ $option->name }}" type="checkbox" value="{{ $option->value }}" class="h-5 w-5 rounded border-gray-500 group-hover:border-gray-700 text-primary-600 focus:ring-primary-600 cursor-pointer">
+                                <label for="wt-{{ $option->name }}" class="pl-4 py-2 select-none w-full text-gray-600 group-hover:text-gray-800 text-lg cursor-pointer">{{ $option->value }}</label>
                             </div>
-                        </div>
-                    </div>
-                    <input type="number" wire:model.live="nDays">
+                        @endforeach
+                    </x-dropdown>
+                    <x-dropdown label="Listed any time">
+                        display options here
+                    </x-dropdown>
                 </div>
             </form>
         </div>
